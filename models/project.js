7f477fec92a1927fef794181aab96b657673962b
jessielista <jmgonzalez8282@gmail.com>
2021-09-22 15:57:21 -0700

    edited schema and updated index and new route

--- models/project.js
+++ models/project.js
@@ -2,31 +2,31 @@ const mongoose = require('mongoose')
 const { Schema, model } = mongoose
 
 const projectsSchema = new Schema({
-  projectName: { type: String, required: true },
+  projectName: { type: String },
   projectDescription: { type: String },
-  dateSubmitted: { type: Date },
-  datePlaced: { type: Date },
+  dateSubmitted: { type: String, default: Date },
+  datePlaced: { type: String, default: Date },
   firmName: { type: String },
-  //   introductionsMade: { type: Boolean, default: false },
-  //   internalContacts: [
-  //     {
-  //       associateName: { type: String },
-  //       email: { type: String },
-  //       phoneNumber: { type: Number },
-  //       title: { type: String },
-  //     },
-  //   ],
-  //   externalContacts: [
-  //     {
-  //       associateName: { type: String },
-  //       email: { type: String },
-  //       phoneNumber: { type: Number },
-  //       title: { type: String },
-  //     },
-  //   ],
-  //   projectStatus: { type: String, required: true },
-  //   legalAgreement: { type: String },
-  //   thankYouLetter: { type: String },
+  introductionsMade: { type: String },
+  // internalContacts: [
+  //   {
+  //     associateName: { type: String },
+  //     email: { type: String },
+  //     phoneNumber: { type: Number },
+  //     title: { type: String },
+  //   },
+  // ],
+  // externalContacts: [
+  //   {
+  //     associateName: { type: String },
+  //     email: { type: String },
+  //     phoneNumber: { type: Number },
+  //     title: { type: String },
+  //   },
+  // ],
+  projectStatus: { type: String },
+  legalAgreement: { type: String },
+  thankYouLetter: { type: String },
 })
 
 const Projects = model('Projects', projectsSchema)
