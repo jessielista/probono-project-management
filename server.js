34b77855c2e64a7c659cdb990ba401cd1d133e1b
jessielista <jmgonzalez8282@gmail.com>
2021-09-28 17:52:54 -0700

    port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 const methodOveride = require('method-override')
 const mongoose = require('mongoose')
